79e768755f3f344803f9bc4fb2aeb4f10f6fdc60
Vedat <vedat.arpa.72@gmail.com>
2025-11-21 13:19:34 +0300

    switch case

---
+++ switchCase.js
@@ -0,0 +1,52 @@
+/* let sayi = prompt('lutfen bir sayı giriniz:');
+
+switch (sayi) {
+  case '1':
+    console.log('Girilen sayi 1 dir');
+    break;
+  case '2':
+    console.log('Girilen sayi 2 dir');
+    break;
+  case '3':
+    console.log('Girilen sayi 3 dür');
+    break;
+  case '4':
+    console.log('Girilen sayi 4 dür');
+    break;
+  default:
+    console.log('Girilen sayi 1 ile 5 arasinda olmalidir');
+    break;
+} */
+
+
+/* ----------ATM APPLİCATİON----------*/
+
+
+/* let endPorcess = true;
+for (let i = 0; i < 4; i++) {
+  let bakiye = 400;
+
+işlem = prompt('for wiev the your money value click 1\nfor withdraw money click 2\nfor add money click 3\n for end your process click 4');
+
+switch (işlem) {
+  case '1':
+    console.log(`Your money amount is ${bakiye}`);
+    break;
+  case '2':
+    let withdraw = prompt('pleas enter how much money you want to withdraw');
+    const moneyValue = bakiye - withdraw;
+    console.log(`currently money amount in your bank account ${moneyValue}`);
+
+    break;
+  case '3':
+    let addMoney = prompt('how much do you want add in your bank account');
+    Nbakiye = addMoney + bakiye;
+    console.log(`Your currently money value is ${bakiye}`);
+    break;
+  case '4':
+    break;
+  default:
+    console.log('Lütfen 1 ile 4 arasinda bir değer giriniz!');
+    break;
+}};
+ */
