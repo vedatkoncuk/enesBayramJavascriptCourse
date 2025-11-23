bc62783bf17de83351bd1fed8cdfd9e536921211
Vedat <vedat.arpa.72@gmail.com>
2025-11-23 11:27:34 +0300

    Diziler ve Metotlar

---
+++ diziler.js
@@ -0,0 +1,36 @@
+/* let sayilarİsimler = [1,2,3,4,5,6,'Vedat',7,8,10,'Koncuk'];
+
+console.log(sayilarİsimler);
+
+let isimler = ['Vedat','Sedat','Boran','Baran'];
+console.log(isimler); */
+
+let cars = ['bmv', 'mercedes', 'renault', 'porcshe'];
+
+/* PUSH METOT */
+/* console.log(cars.length);
+cars.push('opel');
+console.log(cars.length); */
+
+
+/* UNSHİFT METOT */
+/* cars.unshift('hundai');
+console.log(cars.length);
+console.log(cars); */
+
+
+/* POP METOT */
+/* let silinenEleman = cars.pop();
+console.log(cars);
+console.log(silinenEleman); */
+
+
+/* SHİFT METOT */
+/* let silinenEleman = cars.shift();
+console.log(cars);
+console.log(silinenEleman); */
+
+
+/* SPLİCE METOT */
+cars.splice(1,0,'Vedat');
+console.log(cars);
--- metotlar.js
+++ metotlar.js
@@ -20,7 +20,7 @@ cube(16);
 cube(3); */
 
 
-function kontrolEt(yas) {
+/* function kontrolEt(yas) {
   if(yas < 18){
     console.log('Ehtilyet alamazsiniz');
     
@@ -32,4 +32,69 @@ function kontrolEt(yas) {
 
 yas = prompt('Enter your age pleas:');
 
-kontrolEt(yas);
+kontrolEt(yas); */
+
+
+/* ----------KELİME SAYISI BULMA UGULAMASI ---------- */
+
+
+
+/* let metin = 'Javascripte Enes Bayramin eğitimden devam ediyorum';
+
+let harf = prompt('Enter letter:');
+
+let sonuc = harfiBul(harf);
+console.log(`istenen harf metinde ${sonuc} adet var`);
+
+function harfiBul(harf) {
+let sayi = 0;
+  for(let i = 0; i < metin.length; i++){
+    if(metin.charAt(i) === harf){
+      sayi = sayi + 1;
+      console.log('istenen harf bulundu');
+     
+    } else {
+      console.log('istenen harf metinde bulunmamakta!');
+    };
+  };
+  return sayi;
+}; */
+
+
+
+/* ----------MÜKEMMEL SAYİ UGULAMASİ---------- */
+
+
+/* 
+mukemmelSayi(6);
+
+function mukemmelSayi(sayi){
+  let toplam = 0;
+
+  for(let i = 2; i <= sayi / 2; i++){
+    if(sayi % i == 0){
+      toplam += i;
+      
+    }
+    toplam += sayi + 1;
+  }
+  if(toplam == 2 * sayi){
+    console.log(`tebrikler mukemmel sayiyi ${sayi} buldunuz!`);
+  } else{
+    console.log(`Üzgünüm ${sayi} mükemmel sayi değil!`);
+  }
+} */
+
+
+  /* -------- DECİMAL TO BİNARY CONVERSİON ---------- */
+
+/*    function decimaToBinary(sayi){
+    for(let i = 0; i <= 10; i++){
+      if(sayi % 2 == 0){
+        let kalan = 
+      }
+    }
+   }
+ */
+
+   
