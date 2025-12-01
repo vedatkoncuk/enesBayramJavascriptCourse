a9fad1d866e5841deb04d8f7324b497d3851e2ae
Vedat <vedat.arpa.72@gmail.com>
2025-12-01 16:52:34 +0300

    Arrow Function

---
+++ arrowFunctions.js
@@ -0,0 +1,25 @@
+/* function yazdir(){
+  console.log('Merhaba');
+}
+
+yazdir(); */
+
+           /* Arrow Function  */
+/* const yazdirr = () =>{
+  console.log('Merhaba');
+}
+
+yazdirr()
+
+
+const yazdir1 = (isim, lastName)=>{
+  console.log(`yazdir ${isim} ${lastName}`);
+}
+
+yazdir1('Vedat', 'Koncuk'); */
+
+/* const kupAl = (x) => {
+  return x*x*x
+}
+
+console.log(`Değer: ${kupAl(3)}`); */
---
+++ todoListProject.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('#todoAddForm');
+const addInput = document.querySelector('#todoName');
+const todoList = document.querySelector('.list-group');
+const firstCardBody = document.querySelectorAll('.list-gorup')[1];
+const secondCardBody = document.querySelector('#clearButton');
+
+runEvent();
+
