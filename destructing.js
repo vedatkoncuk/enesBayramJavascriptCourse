ff82af31467f7fe6629d38357dcd4d57175ddf39
Vedat <vedat.arpa.72@gmail.com>
2025-12-02 23:51:10 +0300

    Destructing

---
+++ destructing.js
@@ -0,0 +1,29 @@
+/* let langs = ['c#',' c++','JavaScript', 'Python']
+let lang1, lang2, lang3, lang4 = langs
+console.log(lang2);
+
+const set = new Set();
+set.add('vedat',true)
+set.add('Vedat');
+set.add(4)
+set.add([2,5,6,3,9]);
+
+console.log(set.size);
+
+set.delete('Vedat');
+set.delete('4');
+
+console.log(set.size); */
+
+function write(firstName, lastName){
+  console.log(`İsim : ${firstName} ${lastName}`);
+}
+
+write('Vedat', 'Koncuk')
+
+function writee(){
+  console.log('Vedat Koncuk');
+}
+
+writee();
+
